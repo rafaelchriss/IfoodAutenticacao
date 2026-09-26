cfa169d3aa053b1e2482537490df516af225950b
Rafaelchriss <86614504+rafaelchriss@users.noreply.github.com>
2026-09-26 10:00:01 -0300

    Upgrade compileSdk version from 34 to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.rafaelsantos.ifoodautenticacao"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.rafaelsantos.ifoodautenticacao"
