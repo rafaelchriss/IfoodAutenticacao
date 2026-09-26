46f0911702f388a36f9e2c4f54accfeb9d628b49
Rafael Chriss <rafaelwebanalista@gmail.com>
2026-09-26 08:12:09 -0300

    Atualização de componetes para o funcionamento do codeql

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     id("com.google.gms.google-services") version "4.5.0" apply false
 }
