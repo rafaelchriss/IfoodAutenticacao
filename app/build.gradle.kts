c98261a516d80a4a18b1beca38322a253f0e4326
Rafaelchriss <86614504+rafaelchriss@users.noreply.github.com>
2026-09-26 09:48:38 -0300

    Update Firebase Auth dependency in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,5 +56,5 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:34.19.0"))
     implementation("com.google.firebase:firebase-analytics")
-    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-auth")
 }
